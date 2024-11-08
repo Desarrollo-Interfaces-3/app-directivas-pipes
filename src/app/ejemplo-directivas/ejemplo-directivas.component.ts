d0a565ac846c88aaa4a71d96f9df7c9f77fe684e
Desarrollo-Interfaces-3 <desarrollointerfaces3@gmail.com>
2024-11-08 14:44:17 -0500

    ejemplo de directivas

--- src/app/ejemplo-directivas/ejemplo-directivas.component.ts
+++ src/app/ejemplo-directivas/ejemplo-directivas.component.ts
@@ -9,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class EjemploDirectivasComponent {
 
+  mostrarAlumnos = false
+  mostrarCursos = false
+
+  alumnosLista: Alumno = []
+  cursosLista: Curso = []
+
+  verAlumnos():void {
+    this.mostrarAlumnos = true
+    this.alumnosLista.push({id: 1, nombre: "Luis Angel", apellido: "Salvat"})
+    this.alumnosLista.push({id: 2, nombre: "Harold Angel", apellido: "Salvat"})
+    this.alumnosLista.push({id: 3, nombre: "María Angel", apellido: "Salvat"})
+    this.alumnosLista.push({id: 4, nombre: "Julio", apellido: "Salvat"})
+
+  }
+
+  verCursos():void {
+    this.mostrarCursos = true
+  }
+
 }
+
+type Alumno = Array<{id: number; nombre: string; apellido: string}>
+type Curso = Array<{id: number; nombre: string; precio: number}>
+
